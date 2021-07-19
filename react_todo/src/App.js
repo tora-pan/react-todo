0e6419403889d388b704774e792016bd99d72e56
tora_pan <travis.pandos@gmail.com>
2021-07-18 19:19:01 -0700

    inital commit

---
+++ react_todo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>My First Ever React Project</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
